324382c27041f8f8893e67c1ed4249f8739a11ee
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-29 15:39:34 -0600

    create About section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+// import all components
+import React, { useState } from 'react';
 import About from './components/About';
 
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
+  const [thisCategory, setThisCategory] = useState("about")
   return (
     <div>
-      <main>
-        <About></About>
-      </main>
+      <div className="mobile-header">
+        <Header thisCategory= {thisCategory} setThisCatergory={setThisCategory}></Header>
+      </div>
+      <div>
+				<main>{renderCategory()}</main>
+			</div>
+			<div>
+				<Footer></Footer>
+			</div>
     </div>
   );
 }
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,30 @@ import React from 'react';
 function About() {
   return (
     <section>
-      <h1 id="about">Who am I?</h1>
+      <div className="about" id ="about">
+        <h1 className="header">Welcome to my Portfolio</h1>
+      </div>
+      <div className="photo-center">
+				<img
+					src={require('../../assets/images/Reem.jpg')}
+					alt="Reem Ahmed"
+					className="photo"
+				/>
+			</div>
+      <div>
+        <strong>
+        <p>
+          Junior Web Developer specializing in front end development. 
+          Experienced with all stages of the development cycle for dynamic web projects. 
+          Well-versed in numerous programming languages including HTML5, PHP OOP, JavaScript, CSS, MySQL.
+          Strong background in project management and customer relations.
+        </p>
+        <br></br>
+          <p>I do the work I do because I love it. I like the industry. I like the people I meet. I like to talk shop. 
+            I embrace the nerd. I like to make the web a better place. Hopefully we cross paths in the real world someday and we can make it a better place together.
+          </p>
+        </strong>
+      </div>
     </section>
   
   );
