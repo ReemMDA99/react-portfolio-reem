1246e670d0e1de7077bf3aec07208bda66fdb7b4
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-30 12:25:43 -0600

    add styles to header

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
+// import React library
 import React, { useEffect, useState } from "react";
+// import style page
+import './App.css'
 // import all components
-
 import About from './components/About';
 import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 // import logo from './logo.svg';
-// import style page
-import './App.css';
+
 
 function App() {
   useEffect(() => {
+
 		document.title = "Reem Ahmed's Portfolio";
 	}, []);
 
   const [categories] = useState([
     { name: "About", description:"bcvxb"},
-    // {name: "Navigation", description:"saretgf"},
     { name: "Contact", description:"cxvxcvcxv"},
     { name: "Resume", description:"xcvxcvxcvxcv"},
   ]);
   const [thisCategory, setThisCategory] = useState("About");
   
   return (
-      <div className="App">
-        <header
-          categories={categories}
-          thisCategory= {thisCategory} 
-          setThisCategory={setThisCategory}>
-        </header>
-        {thisCategory === "About" && <About />}
-        {/* {thisCategory ==="Navigation" && <Navigation/>} */}
-      </div>
+    <div className="App">
+      <Navigation
+        categories={categories}
+        thisCategory= {thisCategory} 
+        setThisCategory={setThisCategory}>
+      </Navigation>
+      
+      {thisCategory === "About" && <About/>}
+
+    </div>
+     
       /* <div>
 				<main>{renderCategory()}</main>
 			</div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,18 @@ import Navigation from "../Navigation";
 
 function Header(props) {
     const { thisCategory, setThisCategory } = props;
+    return(
+        <header>
+            <h2>Reem's Portfolio</h2>
+            <div>
+                <Navigation
+                    thisCategory={thisCategory} 
+                    setThisCategory={setThisCategory}>
+
+                </Navigation>
+            </div>
+        </header>
+    )
 }
 
 export default Header;
