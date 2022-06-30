81133f6b2232666d5b250a10ac09019f9fe3f5af
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-30 16:49:54 -0600

    create a resume and import pdf to resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import resume from "../../assets/Resume/Resume.pdf";
+import './resume-style.css';
+
+function Resume() {
+    return(
+
+    )
+}
+export default Resume
