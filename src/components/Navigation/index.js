901ccc0e727fd737f946d9ef78b48b03598f4ab1
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-30 08:58:46 -0600

    create Navigation components and export

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './nav-style.css';
+
+function Navigation({
+    categories,
+    setThisCategory,
+    thisCategory,
+}) {
+    return(
+        
+    );
+}
+export default Navigation;
