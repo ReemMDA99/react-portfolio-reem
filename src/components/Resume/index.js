15a2850a44a5ae6011c7abceb43a0ecbe68d923f
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-30 19:01:54 -0600

    download resume feautre

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,9 +2,22 @@ import React from "react";
 import resume from "../../assets/Resume/Resume.pdf";
 import './resume-style.css';
 
+
 function Resume() {
     return(
+        <section>
+            <div className= "align">
+                <div>
+                    <a href={require("../../assets/Resume/Resume.pdf")} download>
+                        <button target="_blank" className="mt-2">
+					        Click here to Download Resume
+			            </button>
+                    </a>
+                </div>
+            </div>
+             
+        </section>
 
-    )
+    );
 }
-export default Resume
+export default Resume;
