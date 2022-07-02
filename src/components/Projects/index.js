80050d5b4e7a63f0bb101cb3741df2fc29529d2d
Reem Ahmed <reem.mda0909@gmail.com>
2022-07-02 15:01:27 -0600

    fetch the data from github api

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,7 +2,18 @@ import React, { useState, useEffect} from "react";
 import "./projects-style.css";
 
 function Projects() {
-     return();
+    const [projects, setProjects] = useState([]);
+    
+    
+    useEffect(() => {
+        fetch("https://api.github.com/users/ReemMDA99/starred?per_page=6")
+        .then((response) =>
+        response.json()
+        ).then((data) => setProjects(data)
+        );
+    }, []);
+    
+    // return();
 }
 
 export default Projects;
